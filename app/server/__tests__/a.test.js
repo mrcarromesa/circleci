cbcaa36ed10674a1983e6e89fc0177785d8ea6b0
Carlos Rodolfo Mengel Santos <mrcarromesa@gmail.com>
2020-06-13 23:39:22 -0300

    teste ok

--- app/server/__tests__/a.test.js
+++ app/server/__tests__/a.test.js
@@ -6,5 +6,5 @@ function soma(a, b) {
 test('if i call soma function with 4 and 5 it shoud return 9', () => {
   const result = soma(5, 4);
 
-  expect(result).toBe(10);
+  expect(result).toBe(9);
 });
